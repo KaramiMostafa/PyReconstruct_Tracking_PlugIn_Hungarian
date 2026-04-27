cef49cde387a160e1f72f037dfac1a2ffc7c0c1a
Dechosenone <mostafa.karami.1994@gmail.com>
2026-04-27 13:48:15 -0400

    feat(api): export track_series from package

--- src/tracking_hungarian/__init__.py
+++ src/tracking_hungarian/__init__.py
@@ -2,6 +2,7 @@ from .roi import ROIFrameTable
 from .costs import combine_costs, iou_1m
 from .matcher import build_cost_matrix, augmented_hungarian, interpret_assignments
 from .tracks import export_lineage_to_mantrack, generate_output_csv
+from .pipeline import HungarianConfig, track_series
 
 __all__ = [
     "ROIFrameTable",
